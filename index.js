9e3ab3757eaa2c9e898534a33666865ebbffb04c
elijahmm17 <elijahmm17@gmail.com>
2019-10-07 12:07:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function mapToNegativize(source) {
+  return source.map(i => i * -1);
+}
+
+function mapToNoChange(source) {
+  return source.map(i => i);
+}
+
+function mapToDouble(source) {
+  return source.map(i => i * 2);
+}
+
+function mapToSquare(source) {
+  return source.map(i => i * i);
+}
+
+function reduceToTotal(source) {
+  source.reduce(0, function(i) {})
+}
